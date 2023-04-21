dcfe44cc1bef35fb3bc5dab5bd8802e26b11c4fe
youthofmay123 <nhathuy39337343@gmail.com>
2023-04-21 20:25:08 +0700

    edit JavaScript/script.js, edit assets/css/login.css

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
-
+const iconclose = document.querySelector('.icon-close');
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
 });
@@ -13,5 +13,8 @@ loginLink.addEventListener('click', ()=> {
 
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
+});
+iconclose.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popdown');
 });
 
